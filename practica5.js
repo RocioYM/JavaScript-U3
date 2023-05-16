2d4037a68e1c1cc3abd2391b2da14a22a53ff8f6
RocioYM <desilverrocio@yahoo.com>
2023-05-15 22:09:50 -0600

    practicas

---
+++ practica5.js
@@ -0,0 +1,12 @@
+//funciones
+function edad(birthYear) {
+    const edad = 2023 - birthYear
+    return edad
+}
+
+const edadPablo = edad(1996)
+console.log(edadPablo)
+
+function potencia(numero) {
+    
+}
